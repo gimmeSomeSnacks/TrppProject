4b3ff76756208537e0462c71d185c574060126ea
Leonid Isakov <64974600+Peygy@users.noreply.github.com>
2024-03-25 19:06:23 +0300

    Chage dirs order
    Relocate front from layout to frontend
    Delete several files (rubbish)

--- layout/kursach/allJS/swap-image.js
+++
@@ -1,6 +0,0 @@
-function swapImage(smallImageSrc, bigImageSrc) {
-    var bigImg = document.getElementById(bigImageSrc);
-    var smallImg = document.getElementById(smallImageSrc);
-    bigImg.src = smallImg.src;
-}
-
