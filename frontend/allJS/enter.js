42797d32899e4a6efe33022a5b87598a7ab94aa5
Mark <noise-04@mail.ru>
2024-04-18 12:04:52 +0300

    фетч запросы регитсрации и авторизации

--- frontend/allJS/enter.js
+++ frontend/allJS/enter.js
@@ -41,4 +41,61 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     $(target).fadeIn(600);
   
   });
-      
+  //ПОСТ ЗАПРОС С РЕГИСТРАЦИИ
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('reg');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы
+
+    const formData = new FormData(form); // Создаем объект FormData из данных формы
+
+    fetch('http://localhost:81', {
+      method: 'POST',
+      body: formData // Устанавливаем данные для отправки
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Произошла ошибка при выполнении запроса');
+      }
+      // return response.text(); // Возвращаем текст ответа
+    })
+    .then(data => {
+      console.log('Успешный ответ от сервера:', data);
+      // Дополнительная обработка ответа
+    })
+    .catch(error => {
+      console.error('Ошибка при выполнении запроса:', error.message);
+    });
+  });
+});
+//ФЕТЧ ЗАПРОС АВТОРИЗАЦИИ 
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('enter'); // Выбор формы по id
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы
+
+    const formData = new FormData(form); // Создаем объект FormData из данных формы
+
+    fetch('http://localhost:81', {
+      method: 'POST',
+      body: formData // Устанавливаем данные для отправки
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Произошла ошибка при выполнении запроса');
+      }
+      // return response.text(); // Возвращаем текст ответа
+    })
+    .then(data => {
+      console.log('Успешный ответ от сервера:', data);
+      // Дополнительная обработка ответа
+    })
+    .catch(error => {
+      console.error('Ошибка при выполнении запроса:', error.message);
+    });
+  });
+});
+
+  
